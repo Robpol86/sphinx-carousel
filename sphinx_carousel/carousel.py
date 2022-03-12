2eb60d3b10c2a6a98553e5487a2ae68e192cbffb
Robpol86 <robpol86@gmail.com>
2022-03-12 17:02:05 -0600

    Initial implementation. (#2)
    Slides-only for now.

--- sphinx_carousel/carousel.py
+++ sphinx_carousel/carousel.py
@@ -4,11 +4,35 @@ https://sphinx-carousel.readthedocs.io
 https://github.com/Robpol86/sphinx-carousel
 https://pypi.org/project/sphinx-carousel
 """
-from typing import Dict
+from typing import Dict, List
 
+from docutils.nodes import Element, image as docutils_image
+from docutils.parsers.rst import Directive
 from sphinx.application import Sphinx
 
 from sphinx_carousel import __version__
+from sphinx_carousel.nodes import CarouselInnerNode, CarouselItemNode, CarouselSlideNode
+
+
+class Carousel(Directive):
+    """Main directive."""
+
+    has_content = True
+
+    def run(self) -> List[Element]:
+        """Main method."""
+        directive_content = Element()
+        directive_content.document = self.state.document
+        self.state.nested_parse(self.content, self.content_offset, directive_content)
+
+        items = []
+        for idx, image in enumerate(directive_content.traverse(docutils_image)):
+            image["classes"] += ["d-block", "w-100"]
+            items.append(CarouselItemNode(idx == 0, "", image))
+
+        inner_div = CarouselInnerNode("", *items)
+        main_div = CarouselSlideNode("", inner_div)
+        return [main_div]
 
 
 def setup(app: Sphinx) -> Dict[str, str]:
@@ -18,5 +42,8 @@ def setup(app: Sphinx) -> Dict[str, str]:
 
     :returns: Extension version.
     """
-    assert app  # TODO remove.
+    app.add_directive("carousel", Carousel)
+    app.add_node(CarouselInnerNode, html=(CarouselInnerNode.html_visit, CarouselInnerNode.html_depart))
+    app.add_node(CarouselItemNode, html=(CarouselItemNode.html_visit, CarouselItemNode.html_depart))
+    app.add_node(CarouselSlideNode, html=(CarouselSlideNode.html_visit, CarouselSlideNode.html_depart))
     return dict(version=__version__)
---
+++ tests/unit_tests/test_docs/test-slides-only/index.rst
@@ -0,0 +1,7 @@
+.. carousel::
+    .. image:: a.jpg
+        :alt: A
+    .. image:: b.jpg
+        :alt: B
+    .. image:: c.jpg
+        :alt: C
