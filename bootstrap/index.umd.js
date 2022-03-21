7a792a989bb4d867906dada3138297eb23822215
Robpol86 <robpol86@gmail.com>
2022-03-21 13:57:38 -0500

    Strip down Bootstrap.
    Comment out unused components.

--- bootstrap/index.umd.js
+++ bootstrap/index.umd.js
@@ -5,30 +5,30 @@
  * --------------------------------------------------------------------------
  */
 
-import Alert from '../node_modules/bootstrap/js/src/alert'
-import Button from '../node_modules/bootstrap/js/src/button'
+// import Alert from '../node_modules/bootstrap/js/src/alert'
+// import Button from '../node_modules/bootstrap/js/src/button'
 import Carousel from '../node_modules/bootstrap/js/src/carousel'
-import Collapse from '../node_modules/bootstrap/js/src/collapse'
-import Dropdown from '../node_modules/bootstrap/js/src/dropdown'
-import Modal from '../node_modules/bootstrap/js/src/modal'
-import Offcanvas from '../node_modules/bootstrap/js/src/offcanvas'
-import Popover from '../node_modules/bootstrap/js/src/popover'
-import ScrollSpy from '../node_modules/bootstrap/js/src/scrollspy'
-import Tab from '../node_modules/bootstrap/js/src/tab'
-import Toast from '../node_modules/bootstrap/js/src/toast'
-import Tooltip from '../node_modules/bootstrap/js/src/tooltip'
+// import Collapse from '../node_modules/bootstrap/js/src/collapse'
+// import Dropdown from '../node_modules/bootstrap/js/src/dropdown'
+// import Modal from '../node_modules/bootstrap/js/src/modal'
+// import Offcanvas from '../node_modules/bootstrap/js/src/offcanvas'
+// import Popover from '../node_modules/bootstrap/js/src/popover'
+// import ScrollSpy from '../node_modules/bootstrap/js/src/scrollspy'
+// import Tab from '../node_modules/bootstrap/js/src/tab'
+// import Toast from '../node_modules/bootstrap/js/src/toast'
+// import Tooltip from '../node_modules/bootstrap/js/src/tooltip'
 
 export default {
-  Alert,
-  Button,
+  // Alert,
+  // Button,
   Carousel,
-  Collapse,
-  Dropdown,
-  Modal,
-  Offcanvas,
-  Popover,
-  ScrollSpy,
-  Tab,
-  Toast,
-  Tooltip
+  // Collapse,
+  // Dropdown,
+  // Modal,
+  // Offcanvas,
+  // Popover,
+  // ScrollSpy,
+  // Tab,
+  // Toast,
+  // Tooltip
 }
