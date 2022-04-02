dce7bdf65192fb6c2ea195696e4e8a7e4ab0a4c3
Robpol86 <robpol86@gmail.com>
2022-04-02 13:41:48 -0500

    Handle duplicate themes. (#61)
    * Handle duplicate themes.
    
    Created parse_themes() to handle building the same theme more than once.
    
    Moved cls.DIRECTORY_PREFIX concat from modify_sphinx_app() into
    parse_themes().
    
    * increase test coverage

---
+++ tests/unit_tests/test_docs/test-multi-theme/test_parse_themes.py
@@ -0,0 +1,21 @@
+"""Tests."""
+import pytest
+
+from sphinx_carousel.unrelated import MultiTheme
+
+
+def test():
+    """Test."""
+    with pytest.raises(IndexError):
+        MultiTheme.parse_themes([])
+
+    assert MultiTheme.parse_themes(["a"]) == ("a", [], [])
+    assert MultiTheme.parse_themes(["a", "b"]) == ("a", ["b"], ["theme_b"])
+    assert MultiTheme.parse_themes(["a", "b", "c"]) == ("a", ["b", "c"], ["theme_b", "theme_c"])
+    assert MultiTheme.parse_themes(["a", "b", "c", "c"]) == ("a", ["b", "c", "c"], ["theme_b", "theme_c", "theme_c2"])
+
+    assert MultiTheme.parse_themes(["a", "b", "c", "d", "c", "c", "c"]) == (
+        "a",
+        ["b", "c", "d", "c", "c", "c"],
+        ["theme_b", "theme_c", "theme_d", "theme_c2", "theme_c3", "theme_c4"],
+    )
