a917498fca6d7278a7ac0f2be729239114b40538
Robpol86 <robpol86@gmail.com>
2022-04-30 16:56:33 -0500

    Switching to sphinx-multi-theme
    Removing out of scope code from project.

--- tests/unit_tests/test_docs/test-multi-theme/single-theme/off/conf.py
+++
@@ -1,6 +0,0 @@
-"""Sphinx test configuration."""
-exclude_patterns = ["_build"]
-extensions = ["sphinx_carousel.carousel"]
-master_doc = "index"
-nitpicky = True
-html_theme = "alabaster"
--- tests/unit_tests/test_docs/test-multi-theme/subdirs/conf.py
+++
@@ -1,8 +0,0 @@
-"""Sphinx test configuration."""
-from sphinx_carousel.multi_theme import select_theme
-
-exclude_patterns = ["_build"]
-extensions = ["sphinx_carousel.carousel"]
-master_doc = "index"
-nitpicky = True
-html_theme = select_theme(["alabaster", "classic"])
